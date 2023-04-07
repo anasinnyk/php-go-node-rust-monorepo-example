62aa2f77def1060c9896accdbf787dfa5badf507
Andrii Nasinnyk <anasinnyk@macpaw.com>
2023-04-07 22:10:51 +0300

    chore: debug php changes

--- projects/php/config/preload.php
+++ projects/php/config/preload.php
@@ -1,5 +1,5 @@
 <?php
-
+// opcache
 if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
